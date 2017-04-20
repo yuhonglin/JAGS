d225cedea72e506ebcdb09410e69a20ecc5856c8
Honglin Yu <yuhonglin1986@gmail.com>
2017-04-20 21:19:38 +1000

    add basic main file and cmake files.
    Next step: try to make the model parser work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 /*
   The main function of JAGS
  */
+#include <string>
 
+using namespace std;
+
+#include "option.hpp"
 
 int main(int argc, char *argv[])
 {
+  // parse option
+  jags::util::Option opt;
+  opt.set_head("This is the JAGS modified by Honglin\n");
+  opt.add_int("--model", "path to the model file");
+  opt.add_bool("--help", "show help information", false);
+  opt.parse(argc, argv, true);
+  if (opt.get_bool("--help")) {
+    opt.dump_help();
+    return 0;
+  }  
+  
+  // First step
+  // parse a graph
+  string model = opt.get_string("--model");
   
   return 0;
 }
--- src/util/logger.hpp
+++ src/util/logger.hpp
@@ -18,31 +18,31 @@
 #include <sstream>
 #include <exception>
 
-
-#include "constant.hpp"
-
 using namespace std;
 
-/** 
- * commonly used log micro, not suggested for direct use
- * 
- * @param L the log level
- * @param M the log message
- */
-#define LOG(L, M) \
-  if ((L) > Logger::getLogLevel()) { } \
-  else { std::stringstream __s; if (L == LL_FATAL) { __s << "(" << __FILE__ << ", " << __LINE__ << ") "; } \
+namespace jags {
+  namespace util {
+
+    /** 
+     * commonly used log micro, not suggested for direct use
+     * 
+     * @param L the log level
+     * @param M the log message
+     */
+#define LOG(L, M)							\
+    if ((L) > Logger::getLogLevel()) { }				\
+    else { std::stringstream __s; if (L == LL_FATAL) { __s << "(" << __FILE__ << ", " << __LINE__ << ") "; } \
       __s << M; Logger::logMessage(L, __s.str()); }
-/** 
- * log micro that only logs once
- * 
- * @param L the log level
- * @param M the log message
- */
-#define LOG_ONCE(L, M) \
-  static bool _LoggedMessage ## __LINE__ = false; \
-  if (((L) > Logger::getLogLevel()) || _LoggedMessage ## __LINE__) { } \
-  else { std::stringstream __s; if (L == LL_FATAL) { __s << "(" << __FILE__ << ", " << __LINE__ << ") "; } \
+    /** 
+     * log micro that only logs once
+     * 
+     * @param L the log level
+     * @param M the log message
+     */
+#define LOG_ONCE(L, M)							\
+    static bool _LoggedMessage ## __LINE__ = false;			\
+    if (((L) > Logger::getLogLevel()) || _LoggedMessage ## __LINE__) { } \
+    else { std::stringstream __s; if (L == LL_FATAL) { __s << "(" << __FILE__ << ", " << __LINE__ << ") "; } \
       __s << M; Logger::logMessage(L, __s.str()); _LoggedMessage ## __LINE__ = true;}
 
 #define LOG_EXCEPT(M) {std::stringstream __s; __s << "(" << __FILE__ << ", " << __LINE__ << ") " << M; throw runtime_error(__s.str());}
@@ -62,15 +62,15 @@ using namespace std;
 #define LOG_METRICS_ONCE(M) LOG_ONCE(LL_METRICS, M)
 #define LOG_DEBUG_ONCE(M)   LOG_ONCE(LL_DEBUG, M)
 
-#define ASSERT(C) \
-  if (!(C)) { LOG(LL_FATAL, #C); }
+#define ASSERT(C)				\
+    if (!(C)) { LOG(LL_FATAL, #C); }
 
-#define ASSERT_MSG(C, M) \
-  if (!(C)) { LOG(LL_FATAL, #C << ": " << M); }
+#define ASSERT_MSG(C, M)				\
+    if (!(C)) { LOG(LL_FATAL, #C << ": " << M); }
 
 #define TODO LOG_FATAL("not implemented yet");
 
-#define DEPRECATED(C) \
+#define DEPRECATED(C)							\
     {LOG_WARNING_ONCE("deprecated code (" << __FILE__ << ", " << __LINE__ << ")"); C;}
 
 #define START_PROGRESS(S, M) Logger::initProgress(S, M)
@@ -78,122 +78,123 @@ using namespace std;
 #define END_PROGRESS         Logger::initProgress()
 #define SET_PROGRESS(P)      Logger::updateProgress(P)
 
-// LogLevel ---------------------------------------------------------------
-//! Verbosity level in logging.
+    // LogLevel ---------------------------------------------------------------
+    //! Verbosity level in logging.
 
-typedef enum _LogLevel {
-    LL_FATAL = 0,  //!< An unrecoverable error has occurred and the code will terminate
-    LL_ERROR,      //!< A recoverable error has occurred, e.g., a missing file. 
-    LL_WARNING,    //!< Something unexpected happened, e.g., a parameter is zero.
-    LL_MESSAGE,    //!< Standard messages, e.g., application-level progress information.
-    LL_STATUS,     //!< Status messages, e.g., image names and sizes during loading.
-    LL_VERBOSE,    //!< Verbose messages, e.g., intermediate performance results.
-    LL_METRICS,    //!< Metrics messages, e.g., detailed process statistics.
-    LL_DEBUG	//!< Debugging messages, e.g., matrix inversion results, etc. 
-} LogLevel;
+    typedef enum _LogLevel {
+      LL_FATAL = 0,  //!< An unrecoverable error has occurred and the code will terminate
+      LL_ERROR,      //!< A recoverable error has occurred, e.g., a missing file. 
+      LL_WARNING,    //!< Something unexpected happened, e.g., a parameter is zero.
+      LL_MESSAGE,    //!< Standard messages, e.g., application-level progress information.
+      LL_STATUS,     //!< Status messages, e.g., image names and sizes during loading.
+      LL_VERBOSE,    //!< Verbose messages, e.g., intermediate performance results.
+      LL_METRICS,    //!< Metrics messages, e.g., detailed process statistics.
+      LL_DEBUG	//!< Debugging messages, e.g., matrix inversion results, etc. 
+    } LogLevel;
 
-// Logger -----------------------------------------------------------------
-//! Message and error logging. This class is not thread-safe in the interest
-//! of not having to flush the log on every message.
-//!
-//! \sa \ref LoggerDoc
-//!
-class Logger
-{
- public:
-    //! callback for fatal errors
-    static void (*showFatalCallback)(const char *message);
-    //! callback for non-fatal errors
-    static void (*showErrorCallback)(const char *message);
-    //! callback for warnings
-    static void (*showWarningCallback)(const char *message);
-    //! callback for status updates
-    static void (*showStatusCallback)(const char *message);
-    //! callback for messages (standard, verbose and debug)
-    static void (*showMessageCallback)(const char *message);
+    // Logger -----------------------------------------------------------------
+    //! Message and error logging. This class is not thread-safe in the interest
+    //! of not having to flush the log on every message.
+    //!
+    //! \sa \ref LoggerDoc
+    //!
+    class Logger
+    {
+    public:
+      //! callback for fatal errors
+      static void (*showFatalCallback)(const char *message);
+      //! callback for non-fatal errors
+      static void (*showErrorCallback)(const char *message);
+      //! callback for warnings
+      static void (*showWarningCallback)(const char *message);
+      //! callback for status updates
+      static void (*showStatusCallback)(const char *message);
+      //! callback for messages (standard, verbose and debug)
+      static void (*showMessageCallback)(const char *message);
 
-    //! callback for displaying progress
-    static void (*showProgressCallback)(const char *status, double p);
+      //! callback for displaying progress
+      static void (*showProgressCallback)(const char *status, double p);
 
- private:
-    static ofstream _log;
-    static LogLevel _logLevel;
-    static LogLevel _lastMessageLevel;
+    private:
+      static ofstream _log;
+      static LogLevel _logLevel;
+      static LogLevel _lastMessageLevel;
 
-    static string _progressStatus;
-    static int _lastProgress;
-    static int _progressLimit;
-    static bool _bRunning;
+      static string _progressStatus;
+      static int _lastProgress;
+      static int _progressLimit;
+      static bool _bRunning;
 
- public:
-    Logger();
-    ~Logger();
+    public:
+      Logger();
+      ~Logger();
 
-    //! open a log file
-    static void initialize(const char *filename,
-        bool bOverwrite = false);
-    //! open a log file with particular logging level
-    static void initialize(const char *filename,
-        bool bOverwrite, LogLevel level);
-    //! set the current verbosity level
-    static inline void setLogLevel(LogLevel level) {
+      //! open a log file
+      static void initialize(const char *filename,
+			     bool bOverwrite = false);
+      //! open a log file with particular logging level
+      static void initialize(const char *filename,
+			     bool bOverwrite, LogLevel level);
+      //! set the current verbosity level
+      static inline void setLogLevel(LogLevel level) {
         _logLevel = level;
-    }
-    //! set the current verbosity level unless already at a lower level
-    //! and return previous log level
-    static inline LogLevel setAtLeastLogLevel(LogLevel level) {
+      }
+      //! set the current verbosity level unless already at a lower level
+      //! and return previous log level
+      static inline LogLevel setAtLeastLogLevel(LogLevel level) {
         const LogLevel lastLevel = _logLevel;
         if (_logLevel < level) _logLevel = level;
         return lastLevel;
-    }
-    //! get the current verbosity level
-    static inline LogLevel getLogLevel() {
+      }
+      //! get the current verbosity level
+      static inline LogLevel getLogLevel() {
         return _logLevel;
-    }
-    //! check whether current verbosity level is above given level
-    static inline bool checkLogLevel(LogLevel level) {
+      }
+      //! check whether current verbosity level is above given level
+      static inline bool checkLogLevel(LogLevel level) {
         return (_logLevel >= level);
-    }
+      }
 
-    //! log a message (see also \p LOG_* macros)
-    static void logMessage(LogLevel level, const string& msg);
+      //! log a message (see also \p LOG_* macros)
+      static void logMessage(LogLevel level, const string& msg);
 
-    // progress feedback
-    static void setRunning(bool bRunning) {
+      // progress feedback
+      static void setRunning(bool bRunning) {
         _bRunning = bRunning;
-    }
-    static bool isRunning() {
+      }
+      static bool isRunning() {
         return _bRunning;
-    }
-    static void initProgress(const char *status = NULL, int maxProgress = 100) {
+      }
+      static void initProgress(const char *status = NULL, int maxProgress = 100) {
         _progressStatus = status == NULL ? string("") : string(status);
         _lastProgress = 0; _progressLimit = maxProgress; updateProgress(0.0);
-    }
-    static void setProgressStatus(const char *status) {
+      }
+      static void setProgressStatus(const char *status) {
         if (status == NULL) {
-            _progressStatus.clear();
+	  _progressStatus.clear();
         } else {
-            _progressStatus = string(status);
+	  _progressStatus = string(status);
         }
         if (showProgressCallback != NULL) {
-            showProgressCallback(_progressStatus.c_str(),
-                (double)_lastProgress / (double)_progressLimit);
+	  showProgressCallback(_progressStatus.c_str(),
+			       (double)_lastProgress / (double)_progressLimit);
         }
-    }
-    static bool incrementProgress() {
+      }
+      static bool incrementProgress() {
         if (_lastProgress < _progressLimit) {
-            _lastProgress += 1;
-            updateProgress((double)_lastProgress / (double)_progressLimit);
+	  _lastProgress += 1;
+	  updateProgress((double)_lastProgress / (double)_progressLimit);
         }
         return _bRunning;
-    }
-    static bool updateProgress(double p) {
+      }
+      static bool updateProgress(double p) {
         if (showProgressCallback != NULL) {
-            showProgressCallback(_progressStatus.c_str(), p);
+	  showProgressCallback(_progressStatus.c_str(), p);
         }
         return _bRunning;
-    }
-};
-
+      }
+    };
+  }
+}
 
 #endif /* _LOGGER_H_ */
