90e06a4d82726a18c5f9a6e47bd493ecf9387b26
Honglin Yu <yuhonglin1986@gmail.com>
2017-04-21 10:29:14 +1000

    parse tree compilation works

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,22 @@
   The main function of JAGS
  */
 #include <string>
+#include <memory>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
 
 using namespace std;
 
+#include "logger.hpp"
 #include "option.hpp"
+
+using namespace jags::util;
+
+#include "compiler/parser_extra.h"
+#include "compiler/ParseTree.h"
+
 
 int main(int argc, char *argv[])
 {
@@ -21,8 +33,23 @@ int main(int argc, char *argv[])
   }  
   
   // First step
-  // parse a graph
+  // parse a model file
   string model = opt.get_string("--model");
+
+  vector<jags::ParseTree*> * pvariables = 0;
+  jags::ParseTree * pdata = 0;
+  jags::ParseTree * prelations = 0;
+  std::string message;
+
+  FILE* mf = std::fopen(model.c_str(), "r");
   
+  int status = parse_bugs(mf, pvariables, pdata, prelations, message);
+
+  if (status != 0) {
+    stringstream ss;
+    ss << "Error parsing model file: " << message;
+    LOG_ERROR(ss.str());
+  }
+
   return 0;
 }
